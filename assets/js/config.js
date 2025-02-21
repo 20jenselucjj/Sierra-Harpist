d1feee2ce09adc157446d122d5591fe3078ba43b
20jenselucjj <123107353+20jenselucjj@users.noreply.github.com>
2025-02-21 15:16:24 -0700

    modified:   assets/js/config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,8 @@
 // Configuration management
 const config = {
     web3forms: {
-        access_key: process.env.WEB3FORMS_ACCESS_KEY || '${WEB3FORMS_ACCESS_KEY}'
+        // Replace this with your actual Web3Forms access key
+        access_key: '031d7316-27c7-4f63-92d5-e47258ac0dd8' // Get your access key from https://web3forms.com/
     },
     cal: {
         api_key: process.env.CAL_API_KEY || '${CAL_API_KEY}'
